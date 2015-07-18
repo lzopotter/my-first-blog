0d9e67a0cd9de907dbeb632a68ca5bafaf872b78
Lauren <lzopotter@gmail.com>
2015-07-18 16:12:44 -0700

    time zone fix

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
     from django.shortcuts import render
